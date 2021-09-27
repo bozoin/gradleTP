59f8fb1ce0aedd17f975f8e0bbd33931c17b8e3e
bozoin <bozoin@gmail.com>
2021-09-27 11:33:46 +0200

    Test de commit super badassss

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import demo.Money;
+
+public class Main {
+    public static void main(String[] args) {
+        Money m1 = new Money(12,"eur");
+        System.out.println(m1.toString());
+
+
+    }
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -4,14 +4,32 @@ public class Money {
     int amount;
     String devise;
 
-    public Money(int amount, String _devise) {
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    public String getDevise() {
+        return devise;
+    }
+
+    public void setDevise(String devise) {
+        this.devise = devise;
+    }
+
+    public Money(int amount, String devise) {
+        this.amount = amount;
+        this.devise = devise;
     }
 
     @Override
     public String toString() {
         return "Money{" +
-                "amount=" + amount + "devise" + devise +
+                "amount=" + amount +
+                ", devise='" + devise + '\'' +
                 '}';
     }
 }
